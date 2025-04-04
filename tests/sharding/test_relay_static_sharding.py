fbebd40889dbe1739c05d30429668cfb1e26e38c
Florin Barbu <barbu_florin_adrian@yahoo.com>
2025-04-04 14:57:30 +0300

    fix

--- tests/sharding/test_relay_static_sharding.py
+++ tests/sharding/test_relay_static_sharding.py
@@ -18,7 +18,7 @@ class TestRelayStaticSharding(StepsSharding):
             else:
                 raise AssertionError("Retrieving messages without subscribing worked!!!")
         except Exception as ex:
-            assert "Failed to publish: Node not subscribed to topic" in str(ex)
+            assert "no subscription found for pubsubTopic" in str(ex)
 
     def test_subscribe_and_publish_on_another_shard(self):
         self.setup_main_relay_nodes(pubsub_topic=self.test_pubsub_topic)
