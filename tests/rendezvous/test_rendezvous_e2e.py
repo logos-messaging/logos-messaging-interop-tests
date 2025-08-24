0cebf45b2192d6f81940413536d88d2b516d89e0
aya <ayahassan2877@gmail.com>
2025-08-24 22:27:23 +0300

    trying different scenarios to make the protocol work

--- tests/rendezvous/test_rendezvous_e2e.py
+++ tests/rendezvous/test_rendezvous_e2e.py
@@ -26,12 +26,10 @@ class TestE2E(StepsRelay):
 
     def test_basic_rendezvous_register_and_discover(self):
         self.node1.start(rendezvous="true", relay="true")
-        node1_enr = self.node1.get_enr_uri()
-
         self.node2.start(rendezvous="true", relay="true")
+        self.node3.start(rendezvous="true")
+        self.node2.add_peers([self.node1.get_multiaddr_with_id(), self.node3.get_multiaddr_with_id()])
         delay(5)
-        self.node1.set_relay_subscriptions([self.test_pubsub_topic])
-        self.node2.set_relay_subscriptions([self.test_pubsub_topic])
-        self.wait_for_autoconnection([self.node1, self.node2], hard_wait=30)
-        discovered = self.node2.get_peers()
+        discovered = self.node3.get_peers()
+        logger.debug(f"Node3 peers count is : {discovered}")
         assert len(discovered) > 0, "No peers discovered via Rendezvous"
